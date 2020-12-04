db32ca225fe717bd851b636ea860f7aa139824c7
Fabrício Zimmerer Murta <fabricio@object.net>
2020-12-04 03:44:31 -0300

    Adjusts platforms to also check architecture.
    The possible .NET 5 platform builds are platform and architecture
    dependent.

--- npmpk/platform.js
+++ npmpk/platform.js
@@ -11,16 +11,26 @@ function  get() {
     case 'freebsd':
     case 'linux':
     case 'openbsd':
-    case 'android':
-      return 'linux';
+      if (os.arch() == 'x64') {
+        return 'linux-x64';
+      }
     case 'darwin':
-    case 'sunos':
-      return 'mac';
+      return 'osx-x64';
     case 'win32':
-      return 'win';
+      if (os.arch() == 'x64') {
+        return 'win-x64';
+      }
+
+      // It would usually be 'x86' but node likes 'x32' according to:
+      // https://nodejs.org/docs/latest-v14.x/api/os.html#os_os_arch
+      if (os.arch() == 'x32') {
+        return 'win-x86';
+      }
   }
+  throw new Error('Unsupported platform/architecture: ' + os.platform() + '/' + os.arch());
 };
 
+// This would force the script to die very early if called from an unsupported OS.
 const name = get();
 
 module.exports = { name };
